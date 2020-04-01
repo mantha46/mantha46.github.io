e12deb851e6a78db86516d782748d53f3b01d934
Vinay Mantha <35571705+mantha46@users.noreply.github.com>
2020-04-01 15:19:54 -0400

    test

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -7,7 +7,7 @@ const overlay = document.querySelector(".overlay");
 /* Looping through images */
 for (var i = 1; i <= 5; i++) {
   const newImage = document.createElement("img");
-  newImage.setAttribute("src", "pic" + i + ".jpg");
+  newImage.setAttribute("src", "images/pic" + i + ".jpg");
   thumbBar.appendChild(newImage);
   newImage.onclick = function(e) {
     const source = e.target.getAttribute("src");
