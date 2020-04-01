f002163ae9e2735841722ebc8e5f156d2756b9a8
Vinay Mantha <35571705+mantha46@users.noreply.github.com>
2020-04-01 15:15:49 -0400

    test

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -1,5 +1,16 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+/* Looping through images */
+for (var i = 1; i <= 5; i++) {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", "pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    const source = e.target.getAttribute("src");
+    display(source);
+  };
+}
